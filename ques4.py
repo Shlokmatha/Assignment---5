63ca6ab4fabc8c770ecae994b767ff675268c28c
Shlokmatha <103404603+Shlokmatha@users.noreply.github.com>
2022-06-19 22:37:30 +0530

    Update ques4.py

--- ques4.py
+++ ques4.py
@@ -1,10 +1,6 @@
-n=5
-for i in range(n):
-    for j in range(i):
-        print ('* ', end="")
-    print('')
-
-for i in range(n,0,-1):
-    for j in range(i):
-        print('* ', end="")
-    print('')
+a = [1, 2, 3, 4, 5, 4, 3, 2, 1]
+for x in a:
+    output = ''
+    for y in range(x):
+        output += '*'
+    print(output)
