5085ae31725f2b8722c5412d8053e585ce6349b2
Shlokmatha <103404603+Shlokmatha@users.noreply.github.com>
2022-07-11 09:43:26 +0530

    Update ques5.py

--- ques5.py
+++ ques5.py
@@ -1,16 +1,13 @@
-def alp_print():
-    row=int(input("Enter the number of rows"))
-    n=0
-    for i in range(0,row+1):
-        for j in range(i):
-            if n==26:
-                n=0
-        else:
-                pass
-            y=chr(65+n)    
-            print(y,end="")
-            n+=1
-        
-        print("")
+row= int(input('Enter the number of rows:'))
+n=0
 
-alp_print()
+for i in range(0, row+1):
+
+    for j in range(i):
+        if n==26:
+            n=0
+        else:
+            pass
+        y=chr(65+n)
+        print(y, end="")
+    print()
