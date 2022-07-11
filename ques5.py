e7b380c35c8592ef2f6c2dc1a84146c7063e7b96
Shlokmatha <103404603+Shlokmatha@users.noreply.github.com>
2022-07-11 10:03:02 +0530

    Update ques5.py

--- ques5.py
+++ ques5.py
@@ -4,10 +4,11 @@ n=0
 for i in range(0, row+1):
 
     for j in range(i):
-        if n==26:
-            n=0
-        else:
-            pass
-        y=chr(65+n)
-        print(y, end="")
+      if n==26:
+        n=0
+      else:
+        pass
+      y=chr(65+n)
+      print(y, end="")
+      n+=1
     print()
